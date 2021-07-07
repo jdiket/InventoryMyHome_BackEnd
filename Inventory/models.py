0401c3c4b7935c9d3cc6d36544302ab505343958
John <john.diket@gmail.com>
2021-07-07 11:44:35 -0400

    initialized app in settings.py and created initial models in app

--- Inventory/models.py
+++ Inventory/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+class InventoryList(models.Model):
+    owner = models.CharField(max_length=200)
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+class Item(models.Model):
+    inventory_list = models.ForeignKey(InventoryList, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+    serial = models.IntegerField(default=0)
+    location = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
