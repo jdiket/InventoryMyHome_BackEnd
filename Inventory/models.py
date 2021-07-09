e3b52681f2edfd9361b300e858a0ff885ff0724f
John <john.diket@gmail.com>
2021-07-09 12:55:00 -0400

    added user authentication for views, need to adjust to allow for login

--- Inventory/models.py
+++ Inventory/models.py
@@ -1,9 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
 class InventoryList(models.Model):
-    owner = models.CharField(max_length=200)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=200)
 
     def __str__(self):
