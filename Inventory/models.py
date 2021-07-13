d92fa00258cc10a1f47df5dc7400a89907d54694
John <john.diket@gmail.com>
2021-07-13 10:52:23 -0400

    model changes and form chages to reflect

--- Inventory/models.py
+++ Inventory/models.py
@@ -6,6 +6,7 @@ from django.contrib.auth.models import User
 class InventoryList(models.Model):
     owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name="inventorylist", null=True)
     name = models.CharField(max_length=200)
+    
 
     def __str__(self):
         return self.name
@@ -13,8 +14,10 @@ class InventoryList(models.Model):
 class Item(models.Model):
     inventory_list = models.ForeignKey(InventoryList, on_delete=models.CASCADE)
     name = models.CharField(max_length=200)
-    serial = models.IntegerField(default=0)
-    location = models.CharField(max_length=200)
+    serial = models.CharField(max_length=200, default=0)
+    where_purchased = models.CharField(max_length=200, default='Unknown')
+    purchase_amount = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    date_purchased = models.DateField(default=None)
 
     def __str__(self):
         return self.name
