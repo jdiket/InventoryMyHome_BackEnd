8542d3f084be0cd246ade9b42463af36681af323
John <john.diket@gmail.com>
2021-07-13 09:46:11 -0400

    updated landing page with site information and changed routing for user registration to go to login

--- registration/views.py
+++ registration/views.py
@@ -9,7 +9,7 @@ def register(response):
         if form.is_valid():
             form.save()
 
-        return redirect("/")
+        return redirect("/login")
     else:
         form = RegistrationForm()
 
